966506cc998f24f8fcbac9eda7e666ed37bc8485
elfajri yassine <yassineelfajri639@gmail.com>
2023-12-04 14:32:58 -0800

    1234

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = (0, None)
-    else:
-        my_tuple = (len(sentence), sentence[:1])
-    return(my_tuple)
+    slen = len(sentence)
+    if slen > 0:
+        return (slen, sentence[0])
+    return (0, None)
